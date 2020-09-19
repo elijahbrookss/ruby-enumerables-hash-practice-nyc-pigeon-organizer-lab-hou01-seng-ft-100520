f94c37d91cf98981c80e0d4a0c48306ff5eee8c1
Elijah Brooks <elijahcbrooks@gmail.com>
2020-09-19 23:10:03 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -3,6 +3,7 @@ def nyc_pigeon_organizer(data)
   data.each_with_object({}) do |(key, value), final| 
     value.each do |inner_key, names|
       names.each do |name|
+        
         if !final[name] then
           final[name] = {}
         end
